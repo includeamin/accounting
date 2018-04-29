70fea6ea8ef508ea12a9fc37f23f19af0ebc5d82
amin jamal <aminjamal10@gmail.com>
2018-04-29 11:03:28 -0700

    init

---
+++ Accounting/AccountingServer/Controllers/RegisterController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AccountingServer.Controllers
+{
+    [Produces("application/json")]
+    [Route("handamde/Register")]
+    public class RegisterController : Controller
+    {
+        // GET: api/Register
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Register/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+        
+        // POST: api/Register
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+            
+
+        }
+        
+        // PUT: api/Register/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+        
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
